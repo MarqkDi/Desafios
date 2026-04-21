1a3bd5dddd4465d430c027a13ed5c4449ee90b07
MarqkDi <arielduartewrk@gmail.com>
2026-04-21 12:38:58 -0300

    Raiz quadrada funcionando com decimais

--- Python/calduladoraCientifica.py
+++ Python/calduladoraCientifica.py
@@ -1,14 +1,19 @@
-import math
 #Estudo independente em python
 #Data: 21/04/26
+import numpy as np
 exit = 'N'
 print(f'{'Bem-Vindo à Calculadora':=^50}')
 print('Feito por: MarqkDi')
 print('=' * 50)
+
 while exit == 'N':
     print('1 - Calculadora Simples')
     print('2 - Calculadora Científica')
     escolhaCalc = int(input('Escolha o tipo de calculadora: '))
+    def coletarValores():
+        valor1 = float(input('Digite o primeiro valor: '))
+        valor2 = float(input('Digite o segundo valor: '))
+        return valor1, valor2
     if escolhaCalc == 1:
         print('1 - Soma')
         print('2 - Subtração')
@@ -17,10 +22,6 @@ while exit == 'N':
         print('5 - Divisão Inteira')
         print('6 - Potenciação')
         escolhaSOp = int(input('Qual tipo de operação deseja?: '))
-        def coletarValores():
-            valor1 = float(input('Digite o primeiro valor: '))
-            valor2 = float(input('Digite o segundo valor: '))
-            return valor1, valor2
         def soma(a,b):
             return a + b
         def sub(a,b):
@@ -38,23 +39,23 @@ while exit == 'N':
             resultado = soma(v1, v2)
             print(f'O resultado da soma é: {resultado:.2f}')
         elif escolhaSOp == 2:
-            v1, v2 = coletarValores
+            v1, v2 = coletarValores()
             resultado = sub(v1, v2)
             print(f'O resultado da subtração é: {resultado:.2f}')
         elif escolhaSOp == 3:
-            v1, v2 = coletarValores
+            v1, v2 = coletarValores()
             resultado = mult(v1, v2)
             print(f'O resultado da multiplicação é: {resultado:.2f}')
         elif escolhaSOp == 4:
-            v1, v2 = coletarValores
+            v1, v2 = coletarValores()
             resultado = div(v1, v2)
             print(f'O resultado da divisão é: {resultado:.2f} \nCom um resto de {v1 % v2}')
         elif escolhaSOp == 5:
-            v1, v2 = coletarValores
+            v1, v2 = coletarValores()
             resultado = divInt(v1, v2)
             print(f'O resultado da divisão inteira é: {resultado}')
         elif escolhaSOp == 6:
-            v1, v2 = coletarValores
+            v1, v2 = coletarValores()
             resultado = poten(v1, v2)
             print(f'O resultado da potenciação é: {resultado:.2f}')
     elif escolhaCalc == 2:
@@ -67,6 +68,18 @@ while exit == 'N':
         print('7 - Seno')
         print('8 - Tangente')
         escolhaCOp = int(input('Qual tipo de operação deseja?: '))
+        if escolhaCOp == 1:
+            def raizQuadrada(a):
+                for i in np.arange(0, a + 1, 0.00001):
+                    if abs(i * i - a) < 0.001:
+                        return i
+                return None
+            valRQ = float(input('Digite o valor que deseja descobrir a Raiz Quadrada: '))
+            resultado = raizQuadrada(valRQ)
+            if resultado is not None:
+                print(f'A Raiz aproximada é: {resultado:.2f}')
+            else:
+                print('Resultado não encontrado')
     else:
         print('Escolha inválida!')
     exit = input('Deseja fechar a aplicação?(Y/N) ').upper()
